7f8efb7c0873c88fab3152badec2c4bb8c1b9b73
Jacob Bly <104948826+CoconutMacaron@users.noreply.github.com>
2022-07-10 09:58:00 -0400

    edited for security reasons

--- dao/DBConnection.java
+++ dao/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
 	private final static String URL = "jdbc:mysql://localhost:3306/pokedex";
 	private final static String USERNAME = "root";
-	private final static String PASSWORD = "coconut";
+	private final static String PASSWORD = "coconut ";
 	private static Connection connection;
 	private static DBConnection instance;
 
